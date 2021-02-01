f9089096e00d9e97849ac651595819874a6b5fab
shi514652697 <ssnegi_4all@yahoo.com>
2021-02-01 14:27:19 -0600

    updated

--- src/main/java/com/test/Junit5Application.java
+++ src/main/java/com/test/Junit5Application.java
@@ -1,4 +1,4 @@
-package com.spring.react;
+package com.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/test/Junit5ApplicationTests.java
+++ src/test/java/com/test/Junit5ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.spring.react;
+package com.test;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
